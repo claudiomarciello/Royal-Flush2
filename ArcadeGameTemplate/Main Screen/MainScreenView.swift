757dddbd2a05d8b8d14024d8d4d05ffdfc3db355
KBrutscher <148461314+KBrutscher@users.noreply.github.com>
2023-12-12 21:43:24 +0100

    added background to mainview

--- ArcadeGameTemplate/Main Screen/MainScreenView.swift
+++ ArcadeGameTemplate/Main Screen/MainScreenView.swift
@@ -29,55 +29,69 @@ struct MainScreenView: View {
     let accentColor: Color = MainScreenProperties.accentColor
     
     var body: some View {
-        VStack(alignment: .center, spacing: 16.0) {
+        
+       ZStack {
+            //added the background to the mainscreen
+            Image(decorative: "BackgroundBlueGradient")
+               .resizable()
+               .scaledToFill()
+               .ignoresSafeArea()
             
-            /**
-             * # PRO TIP!
-             * The game title can be customized to represent the visual identity of the game
-             */
-            Text("\(self.gameTitle)")
-                .font(.title)
-                .fontWeight(.black)
-                .padding(.top, 24.0)
-            
-            Spacer()
-            
-            
-            
-            /**
-             * To customize the instructions, check the **Constants.swift** file
-             */
-            ForEach(self.gameInstructions, id: \.title) { instruction in
-                GroupBox(label: Label("\(instruction.title)", systemImage: "\(instruction.icon)").foregroundColor(self.accentColor)) {
-                    HStack {
-                        Text("\(instruction.description)")
-                            .font(.callout)
-                        Spacer()
+            VStack(alignment: .center, spacing: 16.0) {
+                
+                /**
+                 * # PRO TIP!
+                 * The game title can be customized to represent the visual identity of the game
+                 */
+                Text("\(self.gameTitle)")
+                    .font(.largeTitle)
+                    .fontWeight(.black)
+                    .foregroundColor(Color.black)
+                    .padding(.top, 24.0)
+                
+                Spacer()
+                
+                
+                
+                /**
+                 * To customize the instructions, check the **Constants.swift** file
+                 */
+                ForEach(self.gameInstructions, id: \.title) { instruction in
+                    GroupBox(label: Label("\(instruction.title)", systemImage: "\(instruction.icon)").foregroundColor(self.accentColor)){
+                        HStack {
+                            Text("\(instruction.description)")
+                                .font(.callout)
+                            Spacer()
+                        }
                     }
+                    .border(Color.accentColor, width: /*@START_MENU_TOKEN@*/1/*@END_MENU_TOKEN@*/)
+                    
+                    
                 }
+                
+                Spacer()
+                
+                /**
+                 * Customize the appearance of the **Insert a Coin** button to match the visual identity of your game
+                 */
+                Button {
+                    withAnimation { self.startGame() }
+                } label: {
+                    Text("Go!")
+                        .padding()
+                        .frame(maxWidth: .infinity)
+                }
+                .foregroundColor(.white)
+                .background(self.accentColor)
+                .cornerRadius(10.0)
+                .padding()
+                
             }
-            
-            Spacer()
-            
-            /**
-             * Customize the appearance of the **Insert a Coin** button to match the visual identity of your game
-             */
-            Button {
-                withAnimation { self.startGame() }
-            } label: {
-                Text("Go!")
-                    .padding()
-                    .frame(maxWidth: .infinity)
-            }
-            .foregroundColor(.white)
-            .background(self.accentColor)
-            .cornerRadius(10.0)
+            .padding()
+            .statusBar(hidden: true)
             
         }
-        .padding()
-        .statusBar(hidden: true)
     }
-    
     /**
      * Function responsible to start the game.
      * It changes the current game state to present the view which houses the game scene.
